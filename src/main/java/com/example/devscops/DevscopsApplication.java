57a195bd69fa3263cc972c350f53c9a64d913957
ShamanK93 <158584781+ShamanK93@users.noreply.github.com>
2025-03-18 16:12:51 +0100

    devsops

---
+++ src/main/java/com/example/devscops/DevscopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.devscops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevscopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevscopsApplication.class, args);
+	}
+
+}
